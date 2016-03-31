f879265969d45ac0c21e009b3854c06fd35cb475
ZhenHangZhang <zhanghangzhen@ezpdeMac-mini-2.local>
2016-03-31 09:30:51 +0800

    yy

--- parkingTest/AppDelegate.h
+++ parkingTest/AppDelegate.h
@@ -11,6 +11,17 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property(nonatomic,copy)void (^block)(UIColor *);
+
+
+
+/**
+ * @brief 在状态栏显示 一些Log
+ *
+ * @param string 需要显示的内容
+ * @param duration  需要显示多长时间
+ */
++ (void) showStatusWithText:(NSString *) string duration:(NSTimeInterval) duration;
 
 
 @end
--- parkingTest/AppDelegate.m
+++ parkingTest/AppDelegate.m
@@ -7,8 +7,22 @@
 //
 
 #import "AppDelegate.h"
+#import "ZHZTabBarVC.h"
 
-@interface AppDelegate ()
+#import "WXApi.h"
+#import "payRequsestHandler.h"
+#import <AlipaySDK/AlipaySDK.h>
+
+#import <BaiduMapAPI_Base/BMKBaseComponent.h>
+
+@interface AppDelegate ()<WXApiDelegate,BMKGeneralDelegate>
+{
+           BMKMapManager* _mapManager;
+}
+@property (nonatomic, strong) UIWindow * statusWindow;
+@property (nonatomic, strong) UILabel * statusLabel;
+
+- (void) dismissStatus;
 
 @end
 
@@ -16,8 +30,73 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [self statae];
+    ZHZTabBarVC*tabBarVC = [[ZHZTabBarVC alloc]init];
+    self.window.rootViewController = tabBarVC;
+    // 3.显示窗口(成为主窗口)
+    [self.window makeKeyAndVisible];
+    [WXApi registerApp:APP_ID withDescription:@"Demo"];
+    
+    _mapManager = [[BMKMapManager alloc]init];
+    // 如果要关注网络及授权验证事件，请设定     generalDelegate参数
+    BOOL ret = [_mapManager start:@"BfTFnblpwk1aPwNyGVwWxMvL"  generalDelegate:nil];
+    if (!ret) {
+        NSLog(@"manager start failed!");
+    }
+    
     return YES;
+}
+
+-(void)statae{
+    self.statusWindow = [[UIWindow alloc] initWithFrame:CGRectZero];
+    self.statusWindow.backgroundColor = [UIColor clearColor];
+    self.statusWindow.windowLevel = UIWindowLevelStatusBar + 1;
+    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.statusLabel.backgroundColor = [UIColor blackColor];
+    self.statusLabel.textColor = [UIColor whiteColor];
+    self.statusLabel.font = [UIFont systemFontOfSize:12.0f];
+    self.statusWindow.rootViewController = [[UIViewController alloc]init];
+    [self.statusWindow addSubview:self.statusLabel];
+    [self.statusWindow makeKeyAndVisible];
+}
+/**
+ * @brief 在状态栏显示 一些Log
+ *
+ * @param string 需要显示的内容
+ * @param duration  需要显示多长时间
+ */
++ (void) showStatusWithText:(NSString *) string duration:(NSTimeInterval) duration {
+    
+    AppDelegate * delegate = (AppDelegate *) [UIApplication sharedApplication].delegate;
+    
+    delegate.statusLabel.text = string;
+    [delegate.statusLabel sizeToFit];
+    CGRect rect = [UIApplication sharedApplication].statusBarFrame;
+    CGFloat width = delegate.statusLabel.frame.size.width;
+    CGFloat height = rect.size.height;
+    rect.origin.x = rect.size.width - width - 5;
+    rect.size.width = width;
+    delegate.statusWindow.frame = rect;
+    delegate.statusLabel.frame = CGRectMake(0, 0, width, height);
+    
+    if (duration < 1.0) {
+        duration = 1.0;
+    }
+    if (duration > 4.0) {
+        duration = 4.0;
+    }
+    [delegate performSelector:@selector(dismissStatus) withObject:nil afterDelay:duration];
+}
+
+/**
+ * @brief 干掉状态栏文字
+ */
+- (void) dismissStatus {
+    CGRect rect = self.statusWindow.frame;
+    rect.origin.y -= rect.size.height;
+    [UIView animateWithDuration:0.5 animations:^{
+        self.statusWindow.frame = rect;
+    }];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -40,6 +119,93 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+//处理阿里支付后的结果
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    NSString *urlStr = url.absoluteString;
+    
+    NSLog(@"%@",urlStr);
+    if ([urlStr hasPrefix:@"yoparking://account"])
+    {
+        BOOL isSuccess = [urlStr containsString:@"is_success=T"];
+   
+        return isSuccess;
+    }
+    [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic)
+     {
+         NSLog(@"result = %@",resultDic);
+     }];
+    return [WXApi handleOpenURL:url delegate:self];
+}
+// 重写AppDelegate的handleOpenURL和openURL方法
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return [WXApi handleOpenURL:url delegate:self];
+}
+
+-(void) onReq:(BaseReq*)req
+{
+    if([req isKindOfClass:[GetMessageFromWXReq class]])
+    {
+        // 微信请求App提供内容， 需要app提供内容后使用sendRsp返回
+        NSString *strTitle = [NSString stringWithFormat:@"微信请求App提供内容"];
+        NSString *strMsg = @"微信请求App提供内容，App要调用sendResp:GetMessageFromWXResp返回给微信";
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:strTitle message:strMsg delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        alert.tag = 1000;
+        [alert show];
+    }
+    else if([req isKindOfClass:[ShowMessageFromWXReq class]])
+    {
+        ShowMessageFromWXReq* temp = (ShowMessageFromWXReq*)req;
+        WXMediaMessage *msg = temp.message;
+        
+        //显示微信传过来的内容
+        WXAppExtendObject *obj = msg.mediaObject;
+        
+        NSString *strTitle = [NSString stringWithFormat:@"微信请求App显示内容"];
+        NSString *strMsg = [NSString stringWithFormat:@"标题：%@ \n内容：%@ \n附带信息：%@ \n缩略图:%lu bytes\n\n", msg.title, msg.description, obj.extInfo, msg.thumbData.length];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:strTitle message:strMsg delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+    else if([req isKindOfClass:[LaunchFromWXReq class]])
+    {
+        //从微信启动App
+        NSString *strTitle = [NSString stringWithFormat:@"从微信启动"];
+        NSString *strMsg = @"这是从微信启动的消息";
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:strTitle message:strMsg delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+-(void) onResp:(BaseResp*)resp
+{
+    NSString *strMsg = [NSString stringWithFormat:@"errcode:%d", resp.errCode];
+    NSString *strTitle;
+    
+    if([resp isKindOfClass:[SendMessageToWXResp class]])
+    {
+        strTitle = [NSString stringWithFormat:@"发送媒体消息结果"];
+    }
+    if([resp isKindOfClass:[PayResp class]]){
+        //支付返回结果，实际支付结果需要去微信服务器端查询
+        strTitle = [NSString stringWithFormat:@"支付结果"];
+        
+        switch (resp.errCode) {
+            case WXSuccess:
+                strMsg = @"支付结果：成功！";
+                NSLog(@"支付成功－PaySuccess，retcode = %d", resp.errCode);
+                break;
+                
+            default:
+                strMsg = [NSString stringWithFormat:@"支付结果：失败！retcode = %d, retstr = %@", resp.errCode,resp.errStr];
+                NSLog(@"错误，retcode = %d, retstr = %@", resp.errCode,resp.errStr);
+                break;
+        }
+    }
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:strTitle message:strMsg delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
 }
 
 @end
